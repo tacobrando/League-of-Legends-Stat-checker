3a6b4a5456924df368a837d363871c106f7424d7
tacobrando <matthewytlee2@gmail.com>
2023-10-26 01:27:26 -0700

    added title

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="h-screen flex flex-col justify-center items-center">
+  <div class="h-screen flex flex-col justify-start items-center mt-10">
+    <span class="text-5xl mb-10 text-center">
+      League Of Legends Stats Checker
+    </span>
     <SummonerSearch />
   </div>
 </template>
